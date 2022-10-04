d5586cab8efb876f27ba856be5f3184f9791255b
Zechariah Foxx <zak.foxx@gmail.com>
2022-10-03 20:54:23 -0400

    refactor code in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,16 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
   // find all categories
-  
+  router.get(/api/categories, (req, res) => {
+    res.send(categories)
+  })
   // be sure to include its associated Products
-});
 
-router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  router.get(/api/categories, id, (req, res) => {
+    let category = categories.find((category) => category.id === req.params.id);
+    res.send(category);
+  })
   // be sure to include its associated Products
-});
 
 router.post('/', (req, res) => {
   // create a new category
