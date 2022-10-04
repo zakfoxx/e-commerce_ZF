e25b4dc0482b69baf9a007b81a256d3793d0626f
Zechariah Foxx <zak.foxx@gmail.com>
2022-10-04 18:50:47 -0400

    refactor code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,30 +1,30 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-  // find all categories
-  router.get(/api/categories, (req, res) => {
-    res.send(categories)
-  })
-  // be sure to include its associated Products
+// find all categories
+router.get("/", (req, res) => {
+  res.send(categories);
+});
+// be sure to include its associated Products
 
-  // find one category by its `id` value
-  router.get(/api/categories, id, (req, res) => {
-    let category = categories.find((category) => category.id === req.params.id);
-    res.send(category);
-  })
-  // be sure to include its associated Products
+// find one category by its `id` value
+router.get("/:id", (req, res) => {
+  let category = categories.find((category) => category.id === req.params.id);
+  res.send(category);
+});
+// be sure to include its associated Products
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
